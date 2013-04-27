2848175686f1eb9003a53a08a34f546bca0bf010
ricardo <rimora7@hotmail.com>
2013-04-26 19:49:13 -0500

    enviar datos servidor
    enviar datos servidor

---
+++ api/servidor.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+function enviarRegistro(nombre,telefono,email){
+$.ajax({  type: "POST",  url: "http://igitsoft.com/pgtest.php",  data: "nom="+nombre+"$tel="+telefono+"&ema="+email}).done(function( msg ) {  alert( "Data Saved: " + msg );
+if (msg==1){
+navigator.notification.confirm("Datos guardados satisfactoriamente",function(botones){
+	
+		switch(botones){
+		 case 1:
+		  navigator.notification.beep(5);
+		  break;
+		 case 2:
+		  navigator.notification.vibrate(500);
+		  break;		
+		}	
+},"Titulo","Beep,Vibrar,Salir");
+
+
+
+}else{
+navigator.notification.alert("Los datos no fueron enviados correctamente",null,"Error de registro","Aceptar");	
+	
+}
+
+
+}); 
+	
+	
+	
+	
+}
