4583810240d59a69299b18b20625df6013681a19
ricardo <rimora7@hotmail.com>
2013-05-22 00:30:54 -0500

    pruebabase6
    pruebabase6

--- api/consultas.js
+++ api/consultas.js
@@ -1,28 +1,3 @@
 // consultas
 
 
-function consulta(tx) {
-     tx.executeSql('DROP TABLE IF EXISTS DEMO');
-      tx.executeSql('CREATE TABLE IF NOT EXISTS DEMO (id INTEGER PRIMARY KEY AUTOINCREMENT, Name TEXT NOT NULL, Club TEXT NOT NULL)');        
-	  tx.executeSql('INSERT INTO DEMO(Name,Club) VALUES ("Alexandre Pato", "AC Milan")');        
-	  tx.executeSql('INSERT INTO DEMO(Name,Club) VALUES ("Van Persie", "Arsenal")');
-}
-
-function errorconsulta(err) {
-    alert("Error processing SQL: "+err);
-}
-
-function listo(db) {
-    alert("success!");
-	db.transaction(queryDB,errorconsulta);
-}
-function queryDB(tx){        
-	tx.executeSql('SELECT * FROM DEMO',[],querySuccess,errorCB);    }
-function querySuccess(tx,results){  
- var row = results.rows.item(0);  
-  alert(row['Name']);
-$("#nomcli").text("Nombre:   hola2"+row['Name']);
-$("#salcli").text("Saldo:    $1000.00"+row['Club']);
-	
-
- }
