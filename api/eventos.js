da71d2357dcce352752472bef2d82f074d1fa71d
ricardo <rimora7@hotmail.com>
2013-06-03 21:26:48 -0500

    p
    p

--- api/eventos.js
+++ api/eventos.js
@@ -40,19 +40,86 @@ $(document).ready(function() {
 				  $("select#menu").val("Lunes").selectmenu("refresh");
 				  //$.mobile.changePage($("#datoscli"));	  			  				  
                });
+  /*  $("#bguardacli").tap(function() { 
+	            var nombre = $("#nomnuevocli").val()	
+				var empresa = $("#empnuevocli").val()	
+				var rfc = $("#rfcnuevocli").val()	  	
+				var direccion = $("#dirnuevocli").val()	  	
+				var colonia = $("#colnuevocli").val()	  	
+				var estado = $("#edonuevocli").val()	  	
+				var municipio = $("#munnuevocli").val()	  	
+				var telefono = $("#telnuevocli").val()	  					
+				guardacliente(nombre,empresa,rfc,direccion,colonia,estado,municipio,telefono);
+				 
+               });*/
+			   
     $("#menu").bind("change",function(event,ui){
 		//alert($("#menu").val());
 	    mostrarclientes($("#menu").val());	
 		
 	});
 		
-    $("li").bind("click",function(event,ui) {
+    $("li").live("vclick",function(event){
                   var clavecli = $(this).attr("id");
-				  alert (clavecli);
+				  //alert (clavecli);
 				  mostrarcliente(clavecli);
 				  //$.mobile.changePage($("#datoscli"));	  			  				  
-               });
+    });
+	$("#depositos").tap(function() { 
+                 //var clavecli = $(this).attr("id");
+				  
+				  llamadascxc();
+				  //$.mobile.changePage($("#datoscli"));	  			  				  
+     });				   
+   $("#checkcli").bind("change",function(event){
+				  alert($("#menu").val());
+				  //llamadascxc();
+				  
+	   if ($("#checkcli").prop("checked")){
+		   alert("checado");
+		   
+	   }
+	   else{
+		   alert("NO checado");
+		   
+	   }
+  
+     });	
+	 $( "#colprueba" ).bind("expand",function(event)
+	 {
+		 alert("abre")
+	 });
+	  $( "#colprueba" ).bind("collapse",function(event)
+	 {
+		 alert("cierra")
+	 });
+
+$("#reportes").click(function() { 	 
+	var cadena="555.5";
+	var saldo=0;
+	alert(Number(cadena)+1);
+	
+	
+ $("#gridprueba").empty();
+	    var html = "";
+					 html += "<div class=ui-block-a><strong>Tipo</strong>creado por codigo.</div>";
+					 html += "<div class=ui-block-b><strong>Documento</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+                     html += "<div class=ui-block-c><strong>Vence</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
+					 html += "<div class=ui-block-d><strong>Saldo</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+                     html += "<div class=ui-block-e><strong>Monto</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+
+                  	 html += "<div class=ui-block-a><strong>Tipo</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
+					 html += "<div class=ui-block-b><strong>Documento</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+                     html += "<div class=ui-block-c><strong>Vence</strong> El texto que se ecriba aquí se amoldará a la mitad de pagina.</div>";
+					 html += "<div class=ui-block-d><strong>Saldo</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+                     html += "<div class=ui-block-e><strong>Monto</strong> El texto que se ecriba aquí se amoldará a la otra mitad de pagina.</div>";
+		$("#gridprueba").append(html);  
+
 });
+
+
+
+});//ultimo
 			   
 			   
 
